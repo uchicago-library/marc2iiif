4ad451c23bc6b3f17d6b727b2cfe99874950bb20
Tyler Danstrom <tdanstrom@uchicago.edu>
2018-02-14 11:21:26 -0600

    added marc keyword to setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version="0.1.0",
     license="LGPL3.0",
     description="An application to convert MARC records into IIIF records",
-    keywords="python3.6 iiif-presentation manifests",
+    keywords="python3.6 iiif-presentation manifests marc",
     packages=['marc2iiif'],
     classifiers=[
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
