b0ad1c80c334d07e5e1536d6071b052f5a03bd5d
Tyler Danstrom <tdanstrom@uchicago.edu>
2018-02-14 14:49:03 -0600

    moved bin/tests.py to scripts entrypoint

--- setup.py
+++ setup.py
@@ -9,12 +9,7 @@ setup(
     description="An application to convert MARC records into IIIF records",
     keywords="python3.6 iiif-presentation manifests marc",
     packages=['marc2iiif'],
-    entry_points={
-        'console_scripts': [
-            'test = marc2iiif.test:main'
-        ]
-    },
-    
+    scripts=['bin/test.py'] ,
     classifiers=[
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Development Status :: 5 - Production/Stable",
